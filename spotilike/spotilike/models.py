88b28ae9accdd902725a92e9093c90b9360355a2
astr-id <124276623+astr-id@users.noreply.github.com>
2025-01-14 13:09:59 +0100

    Add initial models and migrations

---
+++ spotilike/spotilike/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Utilisateur(AbstractUser):
+    avatar = models.CharField(max_length=255, null=True, blank=True)
+    biographie = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Artiste(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=45)
+    avatar = models.CharField(max_length=255, null=True, blank=True)
+    biographie = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    titre = models.CharField(max_length=45)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titre
+
+
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    titre = models.CharField(max_length=45)
+    image = models.CharField(max_length=100)
+    date_sortie = models.DateTimeField()
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titre
+
+
+class Morceau(models.Model):
+    id = models.AutoField(primary_key=True)
+    titre = models.CharField(max_length=45)
+    duree = models.TimeField()
+    morceau = models.CharField(max_length=45)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titre
+
+
+class GenreHasMorceau(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    morceau = models.ForeignKey(Morceau, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('genre', 'morceau')
+
+    def __str__(self):
+        return f'{self.genre} - {self.morceau}'
