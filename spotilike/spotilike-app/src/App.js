f975df65dc4d14832287e1805e720ea49f42540b
Chloé Chiarlini <chiarlinipro@gmail.com>
2025-01-15 09:44:03 +0100

    add router

--- spotilike/spotilike-app/src/App.js
+++ spotilike/spotilike-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import Home from "./pages/home";
+import Album from "./pages/albums";
+import Artiste from "./pages/artistes";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/albums" element={<Album />} />
+          <Route path="/artistes" element={<Artiste />} />
+          {/* <Route path="/artistes/:id" element={<Artiste />} />
+          <Route path="/albums/:id" element={<Artiste />} /> */}
+          {/* Route pour les pages non trouvées */}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ spotilike/spotilike-app/src/pages/artistes/artiste.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Artiste() {
+  return <h1>Bienvenue sur la page d'artiste !</h1>;
+}
+
+export default Artiste;
