9b4c897a5583e282de484cb58154d9c625450a54
Chloé Chiarlini <chiarlinipro@gmail.com>
2025-01-15 11:56:32 +0100

    amelioration sidebar

--- spotilike/spotilike-app/src/components/Sidebar.js
+++ spotilike/spotilike-app/src/components/Sidebar.js
@@ -4,25 +4,32 @@ import { FaHome, FaSearch, FaUserAlt, FaMusic } from "react-icons/fa";
 
 function Sidebar() {
   return (
-    <div className="w-64 bg-black p-4">
-      <h1 className="text-2xl font-bold text-white mb-8">Spotify</h1>
-      <ul>
-        <li className="flex flex-col items-center text-gray-300 mb-6 cursor-pointer hover:text-white">
-          <Link to="/">
-            <FaHome className="mr-4" />
-            Accueil
+    <div className="w-64 bg-black p-6 shadow-lg flex flex-col items-center">
+      <img
+        src="../assets/Logo Spotilike.png"
+        alt="Logo"
+        className="w-32 mb-8 object-contain"
+      />
+
+      <h1 className="text-3xl font-bold text-white mb-8">Spotilike</h1>
+
+      <ul className="w-full">
+        <li className="flex items-center text-gray-300 mb-6 cursor-pointer hover:text-white hover:bg-gray-800 p-2 rounded-md transition-all duration-300">
+          <Link to="/" className="flex items-center w-full">
+            <FaHome className="mr-4 text-lg" />
+            <span>Accueil</span>
           </Link>
         </li>
-        <li className="flex items-center text-gray-300 mb-6 cursor-pointer hover:text-white">
-          <Link to="/albums">
-            <FaMusic className="mr-4" />
-            Albums
+        <li className="flex items-center text-gray-300 mb-6 cursor-pointer hover:text-white hover:bg-gray-800 p-2 rounded-md transition-all duration-300">
+          <Link to="/albums" className="flex items-center w-full">
+            <FaMusic className="mr-4 text-lg" />
+            <span>Albums</span>
           </Link>
         </li>
-        <li className="flex items-center text-gray-300 mb-6 cursor-pointer hover:text-white">
-          <Link to="/artistes">
-            <FaUserAlt className="mr-4" />
-            Artistes
+        <li className="flex items-center text-gray-300 mb-6 cursor-pointer hover:text-white hover:bg-gray-800 p-2 rounded-md transition-all duration-300">
+          <Link to="/artistes" className="flex items-center w-full">
+            <FaUserAlt className="mr-4 text-lg" />
+            <span>Artistes</span>
           </Link>
         </li>
       </ul>
